ea95277551b750f6a9a03457200b07ec76fd7281
Salman-Ahamed <me.shahriyarhosen@gmail.com>
2025-05-07 16:40:40 +0600

    Remove console logging from TestPage to comply with ESLint rules and clean up the code.

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,11 +1,5 @@
 import { NewPage } from "@/components/layout";
 
 export const TestPage = () => {
-  console.log("first");
   return <NewPage title="Test Page!" />;
-};
-
-export const test = () => {
-  // Using arrow function instead of function declaration
-  // console.log removed as per ESLint rules
 };
