24b3ba53adfb221b7539aec169009f0851cbdaa1
Salman-Ahamed <me.shahriyarhosen@gmail.com>
2025-05-13 16:48:41 +0600

    refactor: enhance error handling component with improved UI and logging functionality

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,6 +3,5 @@ import { NextPage } from "next";
 import { NewPage } from "@/components/layout";
 
 const TestPage: NextPage = () => <NewPage title="Test Page!" />;
-// const TestPage: NextPage = () => <ErrorPage />;
 
 export default TestPage;
